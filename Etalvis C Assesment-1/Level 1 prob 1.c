d0e02c842c5c8506b06bb71da0aa28727023cca6
Jeyasurya-07 <jeyasuryamuniyandi@gmail.com>
2026-02-05 07:44:26 +0530

    Initial commit

---
+++ Etalvis C Assesment-1/Level 1 prob 1.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+
+int main()
+{
+    int x=0,y=0,result=0;
+    printf("Enter the number:");
+    scanf("%d",&x);
+    y=x+2;
+    
+    printf("Result=%d",y);
+}
---
+++ surys.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("surya");
+
+    return 0;
+}
